8419ac951ed84b0601436fc3c72919c0ccd267e3
Kishore <kishorep16ai@gmail.com>
2025-09-03 09:22:12 +0530

    initial commit

---
+++ Go.Kishore.Events/User/UserUpdatedEvent.cs
@@ -0,0 +1,33 @@
+﻿namespace Go.Kishore.Events.User
+{
+    public record UserUpdatedEvent
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DisplayName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string CountryCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool? IsActive { get; set; }
+        public string Language { get; set; }
+
+        public UserUpdatedEvent()
+        {
+
+        }
+        public UserUpdatedEvent(Guid id, string firstName, string lastName, string displayName, string userName, string email, string countryCode, string phoneNumber, bool? isActive)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            DisplayName = displayName;
+            UserName = userName;
+            Email = email;
+            CountryCode = countryCode;
+            PhoneNumber = phoneNumber;
+            IsActive = isActive;
+        }
+    }
+}
